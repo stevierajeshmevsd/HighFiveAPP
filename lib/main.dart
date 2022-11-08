b121210766ee7dc1a3dd3310ab15edbc8427ec86
Stevierajesh <78890359+Stevierajesh@users.noreply.github.com>
2022-11-08 13:39:00 -0500

    Sets the file system and adds images

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:event_listener/event_listener.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Image Demo'),
-          backgroundColor: Colors.white,
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset('images/high_five_logo_revert.png',
-                  height: 400, width: 250),
-              Image.asset('images/loadinglol.gif', height: 300, width: 250),
-            ],
-          ),
-        ),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    body:
+    Image.asset('images/HIGH_FIVE_LOGO.png'); //there are bugs
+    throw UnimplementedError();
   }
 }

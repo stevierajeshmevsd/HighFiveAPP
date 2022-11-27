aa71ed82edc70014c9f21ed376923f2a6a368947
Stevierajesh <78890359+Stevierajesh@users.noreply.github.com>
2022-11-26 23:54:18 -0500

    Connection between pages
    Finished the buttons, they are able to connect the pages toether, added the pages and almost ready for the demo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:event_listener/event_listener.dart';
+import 'package:flutter_application_1/loading.dart';
+import 'package:flutter_application_1/social.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(home: MyApp1()));
+}
 
-class MyApp extends StatefulWidget {
+class MyApp1 extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    body:
-    Image.asset('images/HIGH_FIVE_LOGO.png'); //there are bugs
-    throw UnimplementedError();
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(children: <Widget>[
+      Image.asset('images/HIGH_FIVE_LOGO.png', height: 200, width: 250),
+      Container(
+        margin: EdgeInsets.all(25),
+        child: FlatButton(
+          child: Text(
+            'Social Media',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          color: Color.fromARGB(220, 255, 112, 112),
+          textColor: Colors.white,
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SocialMedia()));
+          },
+        ),
+      ),
+    ])));
   }
 }
---
+++ lib/social.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+import 'package:event_listener/event_listener.dart';
+
+void main() => runApp(SocialMedia());
+
+class SocialMedia extends StatelessWidget {
+  // This widget is the root of the application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: AppBarBack(),
+    );
+  }
+}
+
+class AppBarBack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: const Alignment(0.0, -0.25),
+              child: SizedBox(
+                width: 100.00,
+                height: 350.00,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  // Put Your Child widget here.
+                ),
+              ),
+            ),
+            Align(
+              alignment: const Alignment(0.0, -1.0),
+              child: Image.asset('images/HIGH_FIVE_LOGO.png',
+                  height: 150, width: 150),
+            ),
+            Align(
+              alignment: const Alignment(0.0, -0.5),
+              child: Image.asset('images/image67.png', height: 60, width: 60),
+            ),
+            Align(
+              alignment: const Alignment(0.0, -0.3),
+              child:
+                  Image.asset('images/TikTok-logo.png', height: 90, width: 90),
+            ),
+            Align(
+              alignment: const Alignment(0.0, -0.02),
+              child: Image.asset('images/Facebook.png', height: 95, width: 95),
+            ),
+            Align(
+              alignment: const Alignment(0.0, 0.21),
+              child: Image.asset('images/high_five_logo_revert.png',
+                  height: 80, width: 80),
+            ),
+            const Align(
+              alignment: Alignment(0.6, 0.3),
+              child: SizedBox(
+                width: 150,
+                child: Text(
+                  "<------Visit Our Website!",
+                ),
+              ),
+            ),
+            Align(
+              alignment: const Alignment(0.0, 0.5),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Color.fromRGBO(255, 122, 158, 1) // foreground
+                    ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyApp1()));
+                },
+                child: const Text('Back'),
+              ),
+            ),
+            Align(
+              alignment: const Alignment(0.0, 1.38),
+              child:
+                  Image.asset('images/marysville.png', height: 300, width: 300),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

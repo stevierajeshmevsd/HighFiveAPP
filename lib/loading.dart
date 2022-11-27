23c1abb7c5ae6236602d9181ed9499d91afd977f
stevierajeshmevsd <98038032+stevierajeshmevsd@users.noreply.github.com>
2022-11-26 23:58:14 -0500

    Revert "Additions to loading screen as well as Creating the socials page."

--- lib/loading.dart
+++ lib/loading.dart
@@ -15,12 +15,9 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: <Widget>[
-              Image.asset('images/HIGH_FIVE_LOGO.png', height: 300, width: 300),
-              Image.network(
-                  'https://th.bing.com/th/id/R.e7a5419cd0ecefde1a3790b31a08d60a?rik=zcWw1%2bK8Wy2p5g&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2f7ca%2f6Rr%2f7ca6RrLAi.gif&ehk=6gTaT20i6gNWvP0MLLV%2bOKeeAvoS05QnWHCD4nJXTks%3d&risl=&pid=ImgRaw&r=0',
-                  height: 100,
-                  width: 100),
-              Image.asset('images/marysville.png', height: 250, width: 250),
+              Image.asset('images/high_five_logo_revert.png',
+                  height: 400, width: 250),
+              Image.asset('images/loadinglol.gif', height: 300, width: 250),
             ],
           ),
         ),

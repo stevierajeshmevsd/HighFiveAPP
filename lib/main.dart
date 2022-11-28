b5c5cbe03c7114743a873688d5591beb04a7df99
Stevierajesh <78890359+Stevierajesh@users.noreply.github.com>
2022-11-27 22:17:45 -0500

    Updated main.dart
    This update finishes the main landing page, needs a little tweaking though.

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,79 @@ class _MyAppState extends State<MyApp1> {
     return Scaffold(
         body: Center(
             child: Column(children: <Widget>[
-      Image.asset('images/HIGH_FIVE_LOGO.png', height: 200, width: 250),
       Container(
-        margin: EdgeInsets.all(25),
-        child: FlatButton(
+        margin: EdgeInsets.only(top: 40),
+        child:
+            Image.asset('images/HIGH_FIVE_LOGO.png', height: 200, width: 240),
+      ),
+      Container(
+        margin: EdgeInsets.only(left: 19, top: 70, right: 19, bottom: 0),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+              margin: EdgeInsets.only(left: 5, right: 5),
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                    side: BorderSide(
+                      width: 1,
+                    ),
+                    primary: Color.fromARGB(220, 255, 112, 112)),
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('New Game'),
+              )),
+          Container(
+              margin: EdgeInsets.only(left: 5, right: 5),
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    side: BorderSide(
+                      width: 1,
+                    ),
+                    primary: Color.fromARGB(220, 255, 112, 112)),
+                icon: const Icon(Icons.arrow_circle_right),
+                label: Text('Continue   '),
+              )),
+        ]),
+      ),
+      Container(
+        margin: EdgeInsets.all(4),
+        child: ElevatedButton(
           child: Text(
-            'Social Media',
+            'Options',
             style: TextStyle(fontSize: 20.0),
           ),
-          color: Color.fromARGB(220, 255, 112, 112),
-          textColor: Colors.white,
+          style: ElevatedButton.styleFrom(
+              side: BorderSide(
+                width: 1,
+              ),
+              primary: Color.fromRGBO(80, 139, 206, 1.0)),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => loading()));
+          },
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(5),
+        child: ElevatedButton(
+          child: Text(
+            'Socials',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          style: ElevatedButton.styleFrom(
+              side: BorderSide(
+                width: 1,
+              ),
+              primary: Color.fromRGBO(80, 139, 206, 1.0)),
           onPressed: () {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => SocialMedia()));
           },
         ),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 90, bottom: 10),
+        child: Image.asset('images/marysville.png', height: 200, width: 200),
       ),
     ])));
   }
